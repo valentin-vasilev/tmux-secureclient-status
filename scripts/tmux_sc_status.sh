2cf54ec546a0e7bb842b8f99ff9fe350579c3bf0
Valentin Vasilev <Valentin.Vasilev@progress.com>
2023-09-18 16:02:48 +0300

    :sparkles: Update status fetch script
    * Remove redundand data fetch to optimize performance
    
    Signed-off-by: Valentin Vasilev <Valentin.Vasilev@progress.com>

--- scripts/tmux_sc_status.sh
+++ scripts/tmux_sc_status.sh
@@ -5,7 +5,6 @@ kernel_name=$(uname -s)
 vpn_status() {
 	if [ "$kernel_name" == "Darwin" ]; then
 		# fetch system network information
-		network_information=$(scutil --nwi)
 		secure_client_stats=$(/opt/cisco/secureclient/bin/vpn stats)
 		if grep -q "Connection State:            Connected" <<< "$secure_client_stats"; then
 		echo " "
