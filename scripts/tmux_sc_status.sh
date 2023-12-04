c210b5f9ffcc66dcb62b5435891d79f8dcdf9693
Valentin Vasilev <Valentin.Vasilev@progress.com>
2023-12-04 22:54:21 +0200

    :bug: Fix issues with missing function
    Signed-off-by: Valentin Vasilev <Valentin.Vasilev@progress.com>

--- scripts/tmux_sc_status.sh
+++ scripts/tmux_sc_status.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$CURRENT_DIR/helpers.sh"
+
 kernel_name=$(uname -s)
 
 vpn_connected_icon=""
 vpn_disconnected_icon=""
 
-vpn_connected_icon_default=""
-vpn_disconnected_icon_default=""
+vpn_connected_icon_default=" "
+vpn_disconnected_icon_default=" "
 
 vpn_ip_address_default=0
 
@@ -25,7 +28,7 @@ vpn_status() {
 		secure_client_stats=$(/opt/cisco/secureclient/bin/vpn stats)
 		if grep -q "Connection State:            Connected" <<< "$secure_client_stats"; then
 			secure_client_ipv4=$(grep "Client Address (IPv4):" <<< "$secure_client_stats" | awk '{ print $4 }')
-			if [$vpn_ip_address_default -eq 1]; then
+			if [ $vpn_ip_address_default -eq 1 ]; then
 				echo "$vpn_connected_icon [$secure_client_ipv4]"
 			else
 				echo "$vpn_connected_icon"
