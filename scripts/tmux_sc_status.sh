c399c883e136011fb43101812847f1cef2bb3b19
Valentin Vasilev <Valentin.Vasilev@progress.com>
2023-12-04 22:35:33 +0200

    :sparkles: Revamp icon display
    * Enable the option to change icon with tmux option
    * Enable the option to toggle IP address display with tmux option
    
    Signed-off-by: Valentin Vasilev <Valentin.Vasilev@progress.com>

--- scripts/tmux_sc_status.sh
+++ scripts/tmux_sc_status.sh
@@ -2,15 +2,36 @@
 
 kernel_name=$(uname -s)
 
+vpn_connected_icon=""
+vpn_disconnected_icon=""
+
+vpn_connected_icon_default=""
+vpn_disconnected_icon_default=""
+
+vpn_ip_address_default=0
+
+update_vpn_icon() {
+	vpn_connected_icon=$(get_tmux_option "@vpn_connected_icon" "$vpn_connected_icon_default")
+	vpn_disconnected_icon=$(get_tmux_option "@vpn_disconnected_icon" "$vpn_disconnected_icon_default")
+}
+
+update_ip_address_display() {
+	ip_address_display=$(get_tmux_option "@vpn_ip_address" "$vpn_ip_address_default")
+}
+
 vpn_status() {
 	if [ "$kernel_name" == "Darwin" ]; then
 		# fetch system network information
 		secure_client_stats=$(/opt/cisco/secureclient/bin/vpn stats)
 		if grep -q "Connection State:            Connected" <<< "$secure_client_stats"; then
 			secure_client_ipv4=$(grep "Client Address (IPv4):" <<< "$secure_client_stats" | awk '{ print $4 }')
-			echo " [$secure_client_ipv4]"
-	else
-		echo " "
+			if [$vpn_ip_address_default -eq 1]; then
+				echo "$vpn_connected_icon [$secure_client_ipv4]"
+			else
+				echo "$vpn_connected_icon"
+			fi
+		else
+		echo "$vpn_disconnected_icon"
 		fi
 	else
 	echo " "
@@ -19,6 +40,8 @@ vpn_status() {
 }
 
 main() {
+	update_vpn_icon
+	update_ip_address_display
 	vpn_status
 }
 
